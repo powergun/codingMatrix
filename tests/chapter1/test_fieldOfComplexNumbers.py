c6c1591529f3645019c79d3adb821f99159246d1
Wei Ning <macgnw@gmail.com>
2017-10-11 02:54:15 +1300

    add chapter 1 examples and practices

---
+++ tests/chapter1/test_fieldOfComplexNumbers.py
@@ -0,0 +1,18 @@
+
+import numbers
+import math
+import cmath
+
+import unittest
+
+
+class TestComplexNumberArithmatics(unittest.TestCase):
+
+    def test_multiply(self):
+        a = 1 + 3j
+        b = -2 + 6j
+        self.assertEqual(-20, a * b)  # 1 * -2 + 1 * 6j + 3j * -2 + 3j * 6j
+
+    def test_eulersFormula(self):
+        self.assertAlmostEqual(0, cmath.exp(1j * math.pi) + 1)  # e ^ (i.pi) + 1 = 0
+
---
+++ tests/chapter1/test_findKey.py
@@ -0,0 +1,36 @@
+
+import unittest
+import string
+
+data = ['10101', '00100', '10101', '01011', '11001', '00011', '01011', '10101', '00100', '11001', '11010']
+
+
+def to_binary(w):
+    return eval('0b{}'.format(w))
+
+
+class Pad(object):
+
+    def __init__(self, mask):
+        self._mask = mask
+
+    def __call__(self, binary):
+        return (binary ^ self._mask) % 27
+
+
+class TestCrackOneTimePad(unittest.TestCase):
+    """
+    P93 Problem 1.5.1
+
+    eve is evil
+
+    the one-time pad function is f(x) = (x ^ 17) % 27
+    """
+    def test_(self):
+        """
+        Uses a brutal force method
+        """
+        for i in xrange(31):
+            print i, ''.join(
+                [(string.ascii_lowercase + ' ')[Pad(i)(to_binary(d))] for d in data]
+            )
