a5e9165abd0ae2dfa9e3db1e41c1a1886b8601c5
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-02-25 14:32:42 +1100

    wip

--- src/pylinearalgebra/mattools/queries.py
+++ src/pylinearalgebra/mattools/queries.py
@@ -9,32 +9,44 @@ def rank(mat):
 
 
 def dim(space):
-    pass
+    return len(bases(space))
 
 
 def bases(space):
-    return []
+    """
+    Params:
+        space: a list of column vectors
+    """
+    m = sympy.Matrix(zip(*space))
+    r, i_pivots = m.rref()
+    return [r[pivot] for pivot in i_pivots]
 
 
-def is_basis(space, vec):
-    return False
+def is_basis(vectors):
+    # are these vectors independent?
+    # see:
+    # https://math.stackexchange.com/questions/421574/how-to-check-if-a-set-of-vectors-is-a-basis
+    m = sympy.Matrix(zip(*vectors))
+    r, i_pivots = m.rref()
+    return r.columns() == len(i_pivots)
 
 
 def row_space(mat):
     m = sympy.Matrix(mat)
-    return m.rowspace()
+    return m.rref()[0].rowspace()
 
 
 def col_space(mat):
     m = sympy.Matrix(mat)
-    return m.columnspace()
+    return m.rref()[0].columnspace()
 
 
 def null_space(mat):
     m = sympy.Matrix(mat)
-    return m.nullspace()
+    return m.rref()[0].nullspace()
 
 
 def left_null_space(mat):
     m = sympy.Matrix(mat)
-    return m.transpose().nullspace()
+    r, _ = m.rref()
+    return r.transpose().nullspace()
